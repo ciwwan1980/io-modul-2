2bd091fa96b9c59189138acbca6e4ea4408f74e9
ciwwan1980 <ajilqado@gmail.com>
2023-12-06 00:09:23 +0100

    write to a file

--- csv2/task1.py
+++ csv2/task1.py
@@ -1,10 +1,20 @@
 # Stores names in a list
-names = []
+# names = []
 
-# Loop to get names from the user
-for _ in range(3):
-    names.append(input("What's your name? "))
+# # Loop to get names from the user
+# for _ in range(3):
+#     names.append(input("What's your name? "))
 
-# Loop to print greetings for each name in sorted order
-for name in sorted(names):
-    print(f"Hello, {name}")
+# # Loop to print greetings for each name in sorted order
+# for name in sorted(names):
+#     print(f"Hello, {name}")
+
+
+
+# Writes to a file
+
+name=input("Whats your name: ")
+
+file=open("name.txt", "w")
+file.write(name)
+file.close()
